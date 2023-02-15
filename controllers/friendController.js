3079623c6d603e0a50dbbddac56ee9bab908d84d
WinnieThomas <winnieantony1988@gmail.com>
2023-02-15 12:12:35 +0000

    added controllers

---
+++ controllers/friendController.js
@@ -0,0 +1,33 @@
+const { User } = require('../models');
+module.exports = {
+
+  //add reactions
+addFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.params.friendId } },
+      {  new: true }
+    )
+      .then((friendData) =>
+        !friendData
+          ? res.status(404).json({ message: 'No video with this id!' })
+          : res.json(friendData)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Remove reaction
+  removeFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: { reactionId: req.params.friendId } } },
+      {  new: true }
+    )
+    .then((friendData) =>
+    !friendData
+      ? res.status(404).json({ message: 'No video with this id!' })
+      : res.json(friendData)
+  )
+  .catch((err) => res.status(500).json(err));
+  },
+};
